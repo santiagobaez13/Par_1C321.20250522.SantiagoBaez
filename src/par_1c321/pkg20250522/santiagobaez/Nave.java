68c3b26010112121e15857ad23453df11db75024
santiagobaez13 <thiagoarias14@gmail.com>
2025-05-22 18:48:17 -0300

    Creacion de clases

---
+++ src/par_1c321/pkg20250522/santiagobaez/Nave.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package par_1c321.pkg20250522.santiagobaez;
+
+/**
+ *
+ * @author Santiago
+ */
+public abstract class Nave {
+    String nombre;
+    int capacidadDeTripulacion;
+    int anioDeLanzamiento;
+    
+    
+    
+    
+    
+}
---
+++ src/par_1c321/pkg20250522/santiagobaez/NaveExploracion.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package par_1c321.pkg20250522.santiagobaez;
+
+/**
+ *
+ * @author Santiago
+ */
+public class NaveExploracion {
+    
+}
