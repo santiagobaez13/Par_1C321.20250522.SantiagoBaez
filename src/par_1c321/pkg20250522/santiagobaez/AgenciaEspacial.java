050f2d68527e17296aedaeb3af1e9843dc44741a
Santiago Baez <121145890+santiagobaez13@users.noreply.github.com>
2025-05-22 21:16:09 -0300

    Update AgenciaEspacial.java
    Error de GIT, HEAD, no pude realizar los commits finales

--- src/par_1c321/pkg20250522/santiagobaez/AgenciaEspacial.java
+++ src/par_1c321/pkg20250522/santiagobaez/AgenciaEspacial.java
@@ -1,13 +1,61 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package par_1c321.pkg20250522.santiagobaez;
 
-/**
- *
- * @author Santiago
- */
+import java.util.*;
+
 public class AgenciaEspacial {
-    
+
+    private List<Nave> naves = new ArrayList<>();
+
+    public void agregarNave(Nave nave) {
+        if (naves.contains(nave)) {
+            System.out.println("Ya existe una nave con ese nombre y año.");
+        } else {
+            naves.add(nave);
+            System.out.println("Nave agregada.");
+        }
+    }
+
+    public void mostrarNaves() {
+        if (naves.isEmpty()) {
+            System.out.println("No hay naves");
+            return;
+        }
+        for (Nave n : naves) {
+            System.out.println(n);
+        }
+    }
+
+    public void iniciarExploracion() {
+        for (Nave n : naves) {
+            n.iniciarMision();
+        }
+    }
+
+    public void ordenarPorNombre() {
+        Collections.sort(naves, new Comparator<Nave>() {
+            @Override
+            public int compare(Nave n1, Nave n2) {
+                return n1.getNombre().compareTo(n2.getNombre());
+            }
+        });
+        mostrarNaves();
+    }
+
+    public void ordenarPorAnio() {
+        Collections.sort(naves);
+        mostrarNaves();
+    }
+
+    public void ordenarPorTripulacion() {
+        Collections.sort(naves, new Comparator<Nave>() {
+            @Override
+            public int compare(Nave n1, Nave n2) {
+                return Integer.compare(n2.getCapacidadDeTripulacion(), n1.getCapacidadDeTripulacion());
+
+            }
+
+        }
+        );
+        mostrarNaves();
+    }
 }
