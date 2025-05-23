d7a590dc4680d8b859df6820ad3125592c421f47
Santiago Baez <121145890+santiagobaez13@users.noreply.github.com>
2025-05-22 21:17:43 -0300

    Update Nave.java
    Error GIT-HEAD, subido manualmente

--- src/par_1c321/pkg20250522/santiagobaez/Nave.java
+++ src/par_1c321/pkg20250522/santiagobaez/Nave.java
@@ -1,20 +1,53 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package par_1c321.pkg20250522.santiagobaez;
 
-/**
- *
- * @author Santiago
- */
-public abstract class Nave {
+public abstract class Nave implements Comparable <Nave> {
     String nombre;
-    int capacidadDeTripulacion;
-    int anioDeLanzamiento;
+    int capacidadTripulacion;
+    int anioLanzamiento;
+
+    public Nave(String nombre, int capacidadDeTripulacion, int anioDeLanzamiento) {
+        this.nombre = nombre;
+        this.capacidadTripulacion = capacidadDeTripulacion;
+        this.anioLanzamiento = anioDeLanzamiento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getAnioDeLanzamiento() {
+        return anioLanzamiento;
+    }
+
+    public int getCapacidadDeTripulacion() {
+        return capacidadTripulacion;
+    }
     
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Nave)) return false;
+        Nave otra = (Nave) obj;
+        return this.nombre.equalsIgnoreCase(otra.nombre) && this.anioLanzamiento == otra.anioLanzamiento;
+    }
+    @Override
+    public int hashCode(){
+        return nombre.toLowerCase().hashCode()+ anioLanzamiento;
+        
+    }
+        @Override
+    public int compareTo(Nave otra) {
+        if (this.anioLanzamiento != otra.anioLanzamiento) {
+            return Integer.compare(otra.anioLanzamiento, this.anioLanzamiento);
+        }
+        return Integer.compare(otra.capacidadTripulacion, this.capacidadTripulacion);
+    }
     
-    
-    
-    
+    public abstract void iniciarMision();
+
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre + ", Tripulación: " + capacidadTripulacion + ", Año: " + anioLanzamiento;
+    }
 }
